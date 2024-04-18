4e894d01e32c5d54b50e8fa805d70b72c73885e3
Miika Alikirri <nykseli@protonmail.com>
2024-04-18 21:58:33 +0300

    Login in and print out messages recieved from server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,61 @@
+use rust_socketio::{ClientBuilder, Payload};
+use serde_json::json;
+
+mod types;
+
 fn main() {
-    println!("Hello, world!");
+    // get a socket that is connected to the admin namespace
+    let socket = ClientBuilder::new("http://127.0.0.1:9000")
+        .namespace("/")
+        // .transport_type(rust_socketio::TransportType::Any)
+        .on("init", |data, _| {
+            if let Payload::Text(mut data) = data {
+                assert!(data.len() == 1);
+
+                // this is stupid, I hate it but I don't know how to get the data ownedship otherwise
+                let init: types::Init = serde_json::from_value(data.swap_remove(0)).unwrap();
+
+                for network in init.networks {
+                    println!("In network: {}", network.name);
+                    for channel in network.channels {
+                        if channel.type_ == "Lobby" {
+                            println!("  In lobby: ");
+                        } else {
+                            println!("  In channel: {}", channel.name);
+                        }
+                        for message in channel.messages {
+                            if let (Some(mode), Some(nick)) = (message.from.mode, message.from.nick)
+                            {
+                                print!("    {mode}{nick}");
+                            } else {
+                                print!("    ~system~");
+                            }
+
+                            if message.type_ != "message" {
+                                println!(": {}", message.type_)
+                            } else {
+                                println!(": {}", message.text)
+                            }
+                        }
+                    }
+                }
+            }
+        })
+        .on("auth:start", |_, client| {
+            let auth = json!({"user":"duck","password":"duck"});
+            client
+                .emit("auth:perform", auth)
+                .expect("Server unreachable");
+        })
+        // .on("error", |err, _| panic!("{:#?}", err))
+        .on_any(|_event, _payload, _| {
+            // println!("{event:#?}");
+            // println!("{payload:#?}");
+        })
+        .connect()
+        .expect("Connection failed");
+
+    std::thread::sleep(std::time::Duration::from_secs(5));
+
+    socket.disconnect().expect("Disconnect failed")
 }
---
+++ src/types.rs
@@ -0,0 +1,67 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct User {
+    // TODO: map mode into serverOptions->prefix
+    // TODO: these should always exists. Fix parsing of lobby messsage
+    pub mode: Option<String>,
+    pub nick: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ChannelMessage {
+    /// Who has sent the message
+    pub from: User,
+    pub gecos: Option<String>,
+    pub hostmask: Option<String>,
+    pub id: u32,
+    // TODO: previews
+    /// Did you send the the message
+    #[serde(rename = "self")]
+    pub self_: bool,
+    pub text: String,
+    // TODO: how to parse and display time?
+    pub time: String,
+    // TODO: enums for type
+    #[serde(rename = "type")]
+    pub type_: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct NetworkChannel {
+    #[serde(rename = "firstUnread")]
+    pub first_unread: i32,
+    pub highlight: i32,
+    pub key: String,
+    pub muted: bool,
+    pub name: String,
+    pub state: i32,
+    pub topic: String,
+    #[serde(rename = "totalMessages")]
+    pub total_messages: u32,
+    // TODO: enums for type
+    #[serde(rename = "type")]
+    pub type_: String,
+    pub unread: i32,
+    // TODO: users list
+    pub messages: Vec<ChannelMessage>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Network {
+    pub channels: Vec<NetworkChannel>,
+    /// Name of the server / network
+    pub name: String,
+    /// Username on the server
+    pub nick: String,
+    // TODO: serverOptions
+    // TODO: status
+    pub uuid: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Init {
+    pub active: i32,
+    pub networks: Vec<Network>,
+    pub token: String,
+}
